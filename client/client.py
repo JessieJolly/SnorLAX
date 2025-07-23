48d3a5da84b9678214a6df9bce65f93aa6d808e7
Jessica Jolly <jessicajolly4416@gmail.com>
2025-07-23 23:44:59 +0530

    session at client end

--- client/client.py
+++ client/client.py
@@ -1,10 +1,37 @@
 # This file contains code to run the [Client]
 import sys
 
+from common.protocol import Protocol
+
 
 class SSHClient:
-     def __init__(self):
-          pass
+    def __init__(self, host='localhost', port=2222):
+        self.host = host
+        self.port = port
+
+    def start_session(self, sock):
+        print("---Started session with Server---")
+        print("---Type 'exit' to quit session---")
+
+        # while loop for session
+        try:
+            while True:
+                command = input("$ ").strip()
+                if command.lower() == "exit":
+                    break
+                Protocol.send_message(sock, "COMMAND", {'command':command})
+                message_type, data = Protocol.receive_message(sock)
+                if message_type == "COMMAND_RESULT":
+                    print(data.get('output', ''))
+                elif message_type == "ERROR":
+                    print(f"Error: {data.get('error', '')}")
+                else:
+                    break
+        except KeyboardInterrupt:
+            print("Session interrupted!")
+        except Exception as e:
+            print("Session error: {e}")
+    
     
 def main():
     if(len(sys.argv)) < 2:
